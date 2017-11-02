b1fbcfb67040708d4d9e2b49d0c1b99aecfeec8f
Cynthia Louis <cynthialouis.dev@gmail.com>
2017-11-02 11:36:26 +0100

    Adding a Twig extension + tagging service

---
+++ src/AppBundle/Twig/MarkdownExtension.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cynthia
+ * Date: 02/11/2017
+ * Time: 11:14
+ */
+
+namespace AppBundle\Twig;
+
+
+use AppBundle\Service\MarkdownTransformer;
+
+class MarkdownExtension extends \Twig_Extension
+{
+    /**
+     * @var MarkdownTransformer
+     */
+    private $markdownTransformer;
+
+    /**
+     * MarkdownExtension constructor.
+     * @param MarkdownTransformer $markdownTransformer
+     */
+    public function __construct(MarkdownTransformer $markdownTransformer)
+    {
+        $this->markdownTransformer = $markdownTransformer;
+    }
+
+
+    /**
+     * @return array
+     */
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter('markdownify', array($this, 'parseMarkdown'))
+        ];
+    }
+
+    /**
+     * @param $str
+     * @return string
+     */
+    public function parseMarkdown($str)
+    {
+        return $this->markdownTransformer->parse($str);
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'app_markdown';
+    }
+}
